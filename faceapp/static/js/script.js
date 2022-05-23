5a2529cac457e6fd0841c8f2b253dbf1d3075cf8
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-05-23 21:15:32 +0530

    removed extra files

--- faceapp/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("top-nav");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
